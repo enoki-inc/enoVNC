d949c183fdfbf04aeb13cb3fc3634a642fb50dbf
Sergio Nahas <sn2865@columbia.edu>
2023-02-08 17:16:40 -0500

    testing switch control bar position

--- app/ui.js
+++ app/ui.js
@@ -93,12 +93,12 @@ const UI = {
             setTimeout(() => window.scrollTo(0, 1), 100);
         }
         // Switch side of control bar
-        UI.toggleControlbarSide();
+        //UI.toggleControlbarSide();
 
         // Restore control bar position
-        //if (WebUtil.readSetting('controlbar_pos') === 'left') {
-            //UI.toggleControlbarSide();
-        //}
+        if (WebUtil.readSetting('controlbar_pos') === 'left') {
+            UI.toggleControlbarSide();
+        }
 
         UI.initFullscreen();
 
